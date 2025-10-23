655bd30c89d954b526b230144fddd8b2f4d9efc7
Olga Garcia <olgarciaq11@gmail.com>
2025-10-23 09:28:45 +0200

    Clase vehiculo creada

---
+++ src/main/java/net/ausiasmarch/servlet/Vehiculo.java
@@ -0,0 +1,8 @@
+package net.ausiasmarch.servlet;
+
+public interface Vehiculo {
+    //Metodo para verificar si el vehiculo está arrancado
+    Boolean isArrancado();
+    //Metodo para arrancar el vehiculo
+    void arrancar();
+}
